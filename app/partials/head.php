12dd0264c954bd54dfe419fd96fad36b12ac8ffd
AU-Covid-19 <76094129+AU-Covid-19@users.noreply.github.com>
2021-08-02 17:52:39 +1000

    Made Navbar Responsive

--- app/partials/head.php
+++ app/partials/head.php
@@ -1,1 +1,3 @@
 <link rel="stylesheet" href="/public/css/header.css">
+
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
--- app/partials/navbar.php
+++ app/partials/navbar.php
@@ -1,25 +1,56 @@
-<div class="header">
-    <h1><a href="/app">Habit Tracker</a></h1>
-    <div class="menu">
-        <p><a href="/app">Dashboard</a></p>
-        <p><a href="/app/calendar.php">Calendar</a></p>
-        <p><a href="/app/habits.php">Habits</a></p>
+<?php 
+    $name = "";
+    if(isset($user->first) == true) {
+        $name = $user->first . " " . $user->last;
+    } else {
+        $name = "User Auth Not Setup";
+    }
+?>
+
+<div class="navbar">
+    <div class="title">
+        <h1>Habit Tracker</h1>
+        <a onclick="toggle()" class="icon">&#9776;</a>
     </div>
-    <div class="dropdown">
-        <p>
-        <?php 
-            if(isset($user->first) == true) {
-                echo $user->first . " " . $user->last;
-            } else {
-                echo "User Auth Not Setup";
-            }
-        ?>
-        </p>
-        <div class="dropdown-content">
-            <ul>
-                <li><a href="#">Account Settings</a></li>
-                <li><a href="/auth/logout.php">Logout</a></li>
-            </ul>
+    <div id="mobile-dropdown" class="drop-hide" style="flex-grow: 1;">
+        <div class="mobile-dropdown">
+            <div class="pages">
+                <ul>
+                    <li><a href="/app">Dashboard</a></li>
+                    <li><a href="/app/habits.php">Habits</a></li>
+                    <li><a href="/app/calendar.php">Calendar</a></li>
+                </ul>
+            </div>
+
+            <div class="account-dropdown">
+                <p id="desktop">
+                    <?php echo $name ?>
+                </p>
+                <div class="dropdown-content">
+                    <ul>
+                        <li><a href="#">Account Settings</a></li>
+                        <li><a href="/auth/logout.php">Logout</a></li>
+                    </ul>
+                </div>
+                <p id="mobile">
+                    <?php echo $name ?>
+                </p>
+            </div>
         </div>
     </div>
+    
 </div>
+
+<script>
+    function toggle() {
+        var element = document.getElementById("mobile-dropdown");
+        if(element.className === "drop-hide") {
+            element.className = "drop-show";
+        } else {
+            element.className = "drop-hide";
+
+        }
+    }
+
+</script>
+
