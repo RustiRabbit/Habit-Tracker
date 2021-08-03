b00df5556c1f91666cc09fa3ebdb76e7645b3157
AU-Covid-19 <76094129+AU-Covid-19@users.noreply.github.com>
2021-08-03 18:59:09 +1000

    Added Error Checking to the homesreen

--- app/index.php
+++ app/index.php
@@ -5,6 +5,7 @@
     // SQL Database
     include("../php/CONFIG.php");
 
+    // Create Helper Functions to create elements
     function generateHabitElement($id, $name) {
         return '<div class="habit card"><h1>' . $name . '</h1><a onclick="completeGoal(this)">Complete!</a></div>';
     }
@@ -16,22 +17,26 @@
     $habits = "";
     $goals = "";
 
+    // Initial SQL Request to get list of user habits 
     $habits_sql = "SELECT * FROM `habits` WHERE `user_id` = " . $_SESSION["id"];
-    $habits_result = $SQL_DB->CreateConnection()->query($habits_sql);
+    $habits_result = $SQL_DB->CreateConnection()->query($habits_sql); // Query Database
 
-    if ($habits_result->num_rows > 0) {
-        while($row = $habits_result->fetch_assoc()) {
+    if ($habits_result->num_rows > 0) { // Check that habits actually exist
+        while($row = $habits_result->fetch_assoc()) { // Loop through the returned rows
             $id = $row["id"];
             $name = $row["name"];
-            $habits .= generateHabitElement($row["id"], $row["name"]);
+            $habits .= generateHabitElement($row["id"], $row["name"]); // Add Habit Element to the Page
             
-            $goals_sql = "SELECT * FROM habits_completed WHERE `habit_id` = " . $id;
-            $goals_result = $SQL_DB->CreateConnection()->query($goals_sql);
-            $goals .= generateGoalElement($id, $name, $goals_result->num_rows);
+            $goals_sql = "SELECT * FROM habits_completed WHERE `habit_id` = " . $id; // Second SQL Request to get indiviual number of times completed
+            $goals_result = $SQL_DB->CreateConnection()->query($goals_sql); // Query the Database
+            if(isset($goals_result->num_rows) != false) {
+                // This means that the habits have actually been completed
+                $goals .= generateGoalElement($id, $name, $goals_result->num_rows);
+            }
         }
     } else {
         $habits = "<p>No habits</p>";
-        $goals = "<p>No goals</p>";
+        $goals = "<p>Looks like you just started!</p>";
     }
     $SQL_DB->CreateConnection()->close();
 ?>
