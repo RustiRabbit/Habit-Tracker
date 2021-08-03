e8846553f841488fd58069ea9f9f74ff6aa1cde3
AU-Covid-19 <76094129+AU-Covid-19@users.noreply.github.com>
2021-08-03 18:36:46 +1000

    Fixed Mobile Bug
    Where you couldn't select the first habit complete box due to the hidden navbar existing over the first couple of elements

--- auth/signup.php
+++ auth/signup.php
@@ -12,30 +12,30 @@
     <body>
         <div class="content">
             <div class="card">
-                    <h1>Habit Tracker</h1>
-                    <h3>Signup</h3>
-                    <form action="" method="POST">
-                        <div class="input">
-                            <p>First Name</p>
-                            <input type="text" id="first_name" name="first_name" value="">
-                        </div>
-                        <div class="input">
-                            <p>Last Name</p>
-                            <input type="text" id="last_name" name="last_name" value="">
-                        </div>
-                        <div class="input">
-                            <p>Email</p>
-                            <input type="text" id="email" name="email" value="">
-                        </div>
-                        <div class="input">
-                            <p>Password</p>
-                            <input type="password" id="password" name="password" value="">
-                        </div>
-                        <div class="button">
-                            <input type="submit" value="Signup">
-                        </div>
-                    </form>
-                    <p>Already have an account? <a href="/auth/login.php">Log in</a></p>
+                <h1>Habit Tracker</h1>
+                <h3>Signup</h3>
+                <form action="" method="POST">
+                    <div class="input">
+                        <p>First Name</p>
+                        <input type="text" id="first_name" name="first_name" value="">
+                    </div>
+                    <div class="input">
+                        <p>Last Name</p>
+                        <input type="text" id="last_name" name="last_name" value="">
+                    </div>
+                    <div class="input">
+                        <p>Email</p>
+                        <input type="text" id="email" name="email" value="">
+                    </div>
+                    <div class="input">
+                        <p>Password</p>
+                        <input type="password" id="password" name="password" value="">
+                    </div>
+                    <div class="button">
+                        <input type="submit" value="Signup">
+                    </div>
+                </form>
+                <p>Already have an account? <a href="/auth/login.php">Log in</a></p>
             </div>
         </div>
         
