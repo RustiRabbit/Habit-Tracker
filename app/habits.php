375f263305c3f0a63564b2d896809653b10bead3
Wesley7604 <88121093+Wesley7604@users.noreply.github.com>
2021-08-05 11:54:27 +1000

    i copied some code

--- app/habits.php
+++ app/habits.php
@@ -2,10 +2,31 @@
     // Check Authentication
     include("partials/auth_check.php");
 
+    // SQL Database
+    include("../php/CONFIG.php");
+
     function createHabit($id, $name) {
         return "<p>ID: " . $id . ", Name: " . $name . "</p>";
     }
     
+    $conn = $SQL_DB->CreateConnection();
+
+    $habits = "";
+
+    // Initial SQL Request to get list of user habits 
+    $habits_sql = "SELECT * FROM `habits` WHERE `user_id` = " . $_SESSION["id"];
+    $habits_result = $SQL_DB->CreateConnection()->query($habits_sql); // Query Database
+
+    if ($habits_result->num_rows > 0) { // Check that habits actually exist
+        while($row = $habits_result->fetch_assoc()) { // Loop through the returned rows
+            $id = $row["id"];
+            $name = $row["name"];
+            $habits .= createHabit($row["id"], $row["name"]); // Add Habit Element to the Page
+        }
+    } else {
+        $habits = "<p>No habits</p>";
+    }
+    $SQL_DB->CreateConnection()->close();
 ?>
 
 <!DOCTYPE>
@@ -23,7 +44,9 @@
             <h1>Habits</h1>
             <a>Add</a>
         </div>
-
+        <?php
+        echo $habits;
+        ?>
     </body>
 
 </html>
