8efe43f2bdc6ea2ff6b11d682ef72e3458a9693d
AU-Covid-19 <76094129+AU-Covid-19@users.noreply.github.com>
2021-08-09 10:52:26 +1000

    Added CSS for Habits Page
    - Along side modal box

--- app/habits.php
+++ app/habits.php
@@ -3,19 +3,20 @@
     include("partials/auth_check.php");
 
     function createHabit($id, $name, $desc) {
-        return "<li>ID: " . $id . ", Name: " . $name . "<p>" . $desc . "</p></li>";
+        return '<div class="habit-card"><h3>' . $name . '</h3><div class="icons"><a onclick="Modal.Open(\'' . $id  .'\', \'' . $name .'\', \'' . $desc .'\')"><i class="fa fa-edit"></i></a><a><i class="fa fa-trash"></i></a></div></div>';
     }
     
 ?>
-
-<!DOCTYPE>
 <html>
     <head>
         <title>Habit Tracker</title>
 
-        <link rel="stylesheet" href="/public/css/pages/habits.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <?php include("partials/head.php") ?>
+        <link rel="stylesheet" href="/public/css/pages/habits.css">
+
+        <script src="/public/js/habits.js"></script>
+
     </head>
     <body>
         <?php include("partials/navbar.php") ?>
@@ -24,15 +25,28 @@
             <a>Add</a>
         </div>
         <div class="habits">
-            <div>
-                <h3>Go for a run</h3>
-                <i class="fa fa-edit"></i> 
-                <i class="fa fa-trash"></i>
-            </div>
             <?php 
-                echo createHabit("1", "WESLEY", "lol man");
+                echo createHabit("2", "Go for a run", "test");
             ?>
-        </ul>
+        </div>
+
+        <div class="popup hide" id="popup">
+            <div class="popup-bg">
+                <div class="popup-top">
+                    <h3>Editing</h3>
+                    <a onclick="Modal.Close()">Close</a>
+                </div>
+                <div class="popup-content">
+                    <p>Name: </p><input id="name" type="text">
+                    <p>Description: </p><input id="description" type="text">
+                </div>
+                <div class="popup-bottom">
+                    <a>Save</a>
+                </div>
+                
+            </div>
+        </div>
+
 
     </body>
 
