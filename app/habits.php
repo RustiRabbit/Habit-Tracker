9fbd11586088a2c7cc7cbed2f655597d5a672bc1
RustiRabbit <19882548+RustiRabbit@users.noreply.github.com>
2021-08-09 10:07:19 +1000

    Started work on the habits page

--- app/habits.php
+++ app/habits.php
@@ -2,8 +2,8 @@
     // Check Authentication
     include("partials/auth_check.php");
 
-    function createHabit($id, $name) {
-        return "<p>ID: " . $id . ", Name: " . $name . "</p>";
+    function createHabit($id, $name, $desc) {
+        return "<li>ID: " . $id . ", Name: " . $name . "<p>" . $desc . "</p></li>";
     }
     
 ?>
@@ -14,7 +14,7 @@
         <title>Habit Tracker</title>
 
         <link rel="stylesheet" href="/public/css/pages/habits.css">
-
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <?php include("partials/head.php") ?>
     </head>
     <body>
@@ -23,6 +23,16 @@
             <h1>Habits</h1>
             <a>Add</a>
         </div>
+        <div class="habits">
+            <div>
+                <h3>Go for a run</h3>
+                <i class="fa fa-edit"></i> 
+                <i class="fa fa-trash"></i>
+            </div>
+            <?php 
+                echo createHabit("1", "WESLEY", "lol man");
+            ?>
+        </ul>
 
     </body>
 
