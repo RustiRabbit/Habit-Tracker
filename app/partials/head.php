b4ff9316d76a0e48f6be023c2f43212f7c2a45b0
Oscar Stickland <test@test.com>
2021-08-21 19:22:07 +1000

    Updated Navigation Bar

--- app/partials/head.php
+++ app/partials/head.php
@@ -1,2 +1,8 @@
 <link rel="stylesheet" href="/public/css/header.css">
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
