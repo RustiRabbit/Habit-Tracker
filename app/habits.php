35c69108f896f50f1f52715c58706d4351e94f51
Oscar Stickland <test@test.com>
2021-08-22 13:47:16 +1000

    Updated Editing Modal

--- app/habits.php
+++ app/habits.php
@@ -53,6 +53,70 @@
             <?php echo $habits; ?>
         </div>
 
+        <div class="modal show">
+            <div class="modal-content">
+                <div class="top">
+                    <h1>Editing</h1>
+                    <a><?php echo file_get_contents("../public/images/icons/close.svg") ?></a>
+                </div>
+                
+                <div class="input">
+                    <h2>Name:</h2>
+                    <div class="text">
+                        <input type="text" placeholder="What do you want to acomplish?">
+                    </div>
+                </div>
+                <div class="input">
+                    <h2>Description:</h2>
+                    <div class="text">
+                        <textarea rows="4" cols="40" placeholder="Describe your habit"></textarea>
+                    </div>
+                </div>
+                <div class="input">
+                    <h2>Frequency:</h2>
+                    <div class="days">
+                        <div class="day">
+                            <input type="checkbox">
+                            <span>M</span>
+                        </div>
+                        <div class="day">
+                            <input type="checkbox">
+                            <span>T</span>
+                        </div>
+                        <div class="day">
+                            <input type="checkbox">
+                            <span>W</span>
+                        </div>
+                        <div class="day">
+                            <input type="checkbox">
+                            <span>T</span>
+                        </div>
+                        <div class="day">
+                            <input type="checkbox">
+                            <span>F</span>
+                        </div>
+                        <div class="day">
+                            <input type="checkbox">
+                            <span>S</span>
+                        </div>
+                        <div class="day">
+                            <input type="checkbox">
+                            <span>S</span>
+                        </div>
+                    </div>
+                </div>
+            
+                <div class="bottom">
+                    <a href="#" id="delete">
+                        Delete
+                    </a>
+                    <a href="#" id="save">
+                        Save
+                    </a>
+                </div>
+            </div>
+        </div>
+
     </body>
 
 </html>
