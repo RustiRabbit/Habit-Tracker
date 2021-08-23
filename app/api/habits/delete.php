b2a81cca47cfa72190322661406613a8ea6c3ea9
Wesley7604 <88121093+Wesley7604@users.noreply.github.com>
2021-08-23 10:50:00 +1000

    restdyfighkj

--- app/api/habits/delete.php
+++ app/api/habits/delete.php
@@ -1,34 +1,20 @@
 <?php 
-    // This API endpoint deletes a habit
-
-     // THIS IS AN API PAGE - NO HTML
-
     // Include Authentication
     include("../../partials/auth_check.php");
     
     // Include Config
     include("../../../php/CONFIG.php");
 
-    /* 
-        Register GET Params
-        habit_id
-    */
     $habit_id = $_GET["habit_id"];
 
     // Create SQL Connection
     $conn = $SQL_DB->CreateConnection();
-    /*
-        Create SQL Query and Save in variable
-        Reference -> https://www.w3schools.com/sql/sql_delete.asp
+    $delete_query = "DELETE FROM `habits` WHERE `user_id` = " . $user->id . " AND `id` = " . $habit_id;
 
-        Include WHERE condition for user_id - make sure that only the right person can access the database
-    */
-
-    // Create Result variable that runs SQL Query eg: $conn->query(SQL)
-
-    // Check if the result variable equals true;
-    // https://www.w3schools.com/php/php_mysql_delete.asp - Look at example (bottom 10ish lines)
-
-    // If result is true echo "ok"
-    // If result is else echo "error"
+    // Check if SQL query worked
+    if ($conn->query($delete_query) === TRUE) {
+        echo "Ok";
+        } else {
+        echo "Error" . $conn->error;
+        }
 ?>
