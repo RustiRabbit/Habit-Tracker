5e531aa62f968de284bb4ca85f6fdbad390ff40e
Oscar Stickland <test@test.com>
2021-09-05 18:33:52 +1000

    Updated Calendar Styling

--- public/calendar/src/components/Navigation.js
+++ public/calendar/src/components/Navigation.js
@@ -28,23 +28,24 @@ export default function Navigation() {
 
     return (
         <div className="nav">
-            <div className="left">
+            <div className="top">
+                <h1>{Display}</h1>
+            </div>
+            <div className="bottom">
                 <div onClick={() => Decrement()}>
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
+                    <span>Previous Month</span>
                 </div>
                 <div onClick={() => Today()}>
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 3h-1V1h-2v2H7V1H5v2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 18H4V10h16v11zm0-13H4V5h16v3z"/></svg>
+                    <span>Current Month</span>
                 </div>
                 <div onClick={() => Increment()}>
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8-8-8z"/></svg>
+                    <span>Next Month</span>
                 </div>
             </div>
-            <div className="center">
-                <h1>{Display}</h1>
-            </div>
-            <div className="right">   
-                <HabitSelect />
-            </div>
+
         </div>
     )
 }
